cc9d529afe141557b5f1757e7c8106135531bcb8
shantisharma103 <shantisharma103@gmail.com>
2022-09-12 08:23:23 +0530

    upda6te

--- array/ArrayPro.java
+++ array/ArrayPro.java
@@ -3,7 +3,7 @@ class ArrayPro
 	public static void main(String arg[])
 	{
  		int x[]={10,20,30};
-		for(int 0=1; i<x.length; i++)
+		for(int i=0; i<x.length; i++)
 	
 		{
 		System.out.println(x[i]);
