999e3ed6b37584c57fdedf517e167cec7ae55384
shantisharma103 <shantisharma103@gmail.com>
2022-09-16 22:18:54 +0530

    updatedprogram
    updatedprogram

--- Child.java
+++ Child.java
@@ -1,20 +1,22 @@
-interface my
+interface LambdaProgram
 {
 	int sum(int a ,int b);
 }
-	class child
-	{
+class Child
+{
 	public static void main(String args[])
 	{
 		int x =10; 
 		//using lambda exp
-		my m1=(a, b)->(a+b);
-				{
-					System.out.println(m1.sum(10,20));			
-				};
+		LambdaProgram m1=(a, b)->(a+b);
+				
+		System.out.println(m1.sum(10,20));			
+						
 		//multiple parameter with datatype inlambda exp
-		my m2=(int a,int b)->(a+b);
+			
+		LambdaProgram m2=(int a,int b)->(a+b);
+			
 		System.out.println(m2.sum(1,2));			
 	}
-	}
+}
 		
