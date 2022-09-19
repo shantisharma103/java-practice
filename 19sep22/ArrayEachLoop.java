26817f08b37b54ba8e59f0cc7decb7f7c704953e
shantisharma103 <shantisharma103@gmail.com>
2022-09-20 01:23:26 +0530

    19sep22
    array prog

---
+++ 19sep22/ArrayEachLoop.java
@@ -0,0 +1,14 @@
+class ArrayEachLoop
+{
+public static void main(String args[])
+	{
+		int[] x={10,20,30};
+		
+		for(int i:x)
+		{
+			System.out.print(i+" , ");
+		}
+	}
+}
+
+		
---
+++ 19sep22/ArrayPrint.java
@@ -0,0 +1,10 @@
+class ArrayPrint
+{ 
+	public static void main(String args[])
+	{
+		int[] a={10,20,30};
+		
+		System.out.println(a);
+		
+	}
+}
